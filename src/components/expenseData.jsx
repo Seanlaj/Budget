9a7e70e6523d57cc14467de4e674bd7d4d030354
Sean LaJeunesse <Sean.LaJeunesse@yc.edu>
2024-10-02 14:03:42 -0700

    removed two state update functions to test

--- src/components/expenseData.jsx
+++ src/components/expenseData.jsx
@@ -4,24 +4,24 @@ export function getTableData(setExpenseData, month, year, setMonth, setYear) {
     if (month !== null && year !== null) {
       let dataCall = fetch(`https://script.google.com/macros/s/AKfycbxkfSy9HtJyZSMTp9lyz-nlNaGzjTHuNkooM-UShLLIoVw9AZgrZO2wEgEXVv-F0tG7/exec?month=${month}&year=${year}`);
 
+      const saveButton = document.getElementById("saveExpense");
+      saveButton.style.display = "inline";
+
+      const savingButton = document.getElementById("expenseSaving");
+      savingButton.style.display = "none";
+
       dataCall.then(res => res.json()).then(res => {
 
         const expenseArray = formatData(res);
-
-        const saveButton = document.getElementById("saveExpense");
-        saveButton.style.display = "inline";
-
-        const savingButton = document.getElementById("expenseSaving");
-        savingButton.style.display = "none";
 
         setExpenseData(expenseArray);
 
         let newMonth = new Date(Date.parse(month + 1,)).getMonth();
 
-        // if (month !== null && year !== undefined) {
-        //   setMonth(newMonth);
-        //   setYear(year);
-        // }
+        if (month !== null && year !== undefined) {
+          setMonth(newMonth);
+          setYear(year);
+        }
       });
     } else {
       let dataCall = fetch("https://script.google.com/macros/s/AKfycbxkfSy9HtJyZSMTp9lyz-nlNaGzjTHuNkooM-UShLLIoVw9AZgrZO2wEgEXVv-F0tG7/exec");
